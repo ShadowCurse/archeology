f2dc3b1c9e819cdd31e7a7ed18cef56600944346
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2025-12-09 20:30:39 +0000

    reduce number of iterations in the streaming_write test

--- src/streaming_write.zig
+++ src/streaming_write.zig
@@ -12,35 +12,42 @@ const TESTS = [_]testing.Description{
 const SINGLE_READ_SIZE = 256;
 const SOURCE_SIZE = 1 << 20;
 const DEST_SIZE = 1 << 30;
+const NUMBER_OF_TESTS = 128;
 
 pub fn main() !void {
-    var results: [128][2]testing.Result = undefined;
+    var results: [NUMBER_OF_TESTS][2]testing.Result = undefined;
     const source = try testing.create_input(SOURCE_SIZE, .pattern_all_0);
     var inner_loop_count: u64 = 1;
-    while (inner_loop_count * SINGLE_READ_SIZE < SOURCE_SIZE) : (inner_loop_count += 1) {
+    var i: u64 = 0;
+    while (inner_loop_count * SINGLE_READ_SIZE < SOURCE_SIZE and i < NUMBER_OF_TESTS) : ({
+        inner_loop_count *= 2;
+        i += 1;
+    }) {
         const outer_loop_count = DEST_SIZE / (inner_loop_count * SINGLE_READ_SIZE);
         const additional_data = [_]u64{
             outer_loop_count,
             inner_loop_count,
             @intFromPtr(source.ptr),
         };
-        results[inner_loop_count] = try testing.run(
+        results[i] = try testing.run(
             &[_]testing.Description{
                 .{ .function = standard_write, .name = "standard_write" },
                 .{ .function = streaming_write, .name = "streaming_write" },
             },
             @ptrCast(&additional_data),
-            &.{
-                .{ .name = "write_size", .value = inner_loop_count * SINGLE_READ_SIZE },
-            },
+            &.{.{ .name = "write_size", .value = inner_loop_count * SINGLE_READ_SIZE }},
         );
     }
     testing.print_header(&.{.{ .name = "write_size" }});
     inner_loop_count = 1;
-    while (inner_loop_count * SINGLE_READ_SIZE < SOURCE_SIZE) : (inner_loop_count += 1)
+    i = 0;
+    while (inner_loop_count * SINGLE_READ_SIZE < SOURCE_SIZE and i < NUMBER_OF_TESTS) : ({
+        inner_loop_count *= 2;
+        i += 1;
+    })
         testing.print_results(
             &TESTS,
-            &results[inner_loop_count],
+            &results[i],
             &.{.{ .value = inner_loop_count * SINGLE_READ_SIZE }},
         );
 }
